7d0fd60aad70012933097f111a1ff815ee122e02
Sokhomdata <tangchea86@gmail.com>
2026-08-03 15:21:47 +0200

    Configuration environnement Python et test environnement

---
+++ tests/test_environment.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import xgboost
+
+
+print("Pandas :", pd.__version__)
+print("Numpy :", np.__version__)
+print("Scikit-learn :", sklearn.__version__)
+print("XGBoost :", xgboost.__version__)
+
+print("Environnement OK")
