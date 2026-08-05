e024ab9d36d1596c849a5088d0fa9ab69e127498
Sokhom CHEA <tangchea@yahoo.fr>
2026-08-05 07:27:38 +0000

    ajout de fact_shot et mise a jour des stg

--- dbt/models/facts/facts_shots.sql
+++ dbt/models/facts/facts_shots.sql
@@ -1,1 +1,20 @@
+SELECT
+    event_id as shot_id,
+    match_id,
+    player_id as striker_id,
+    team_id,
+    recipient_id as goalkeeper_id,
+    event_id,
+    x,
+    y,
+    body_part_id,
+    xg,
+    outcome_id,
+    under_pressure,
+    first_time,
+    shot_type_id,
+    play_pattern_id,
+    technique_id
 
+FROM {{ ref('stg_events') }}
+WHERE type_name = 'Shot'
